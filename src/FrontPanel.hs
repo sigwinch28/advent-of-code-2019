ae6577e92ea8fe727baf2aa22b3c187510ac457b
Joe Harrison <joe@sigwinch.uk>
2019-12-03 17:32:43 +0000

    day 3 task 2

--- src/FrontPanel.hs
+++ src/FrontPanel.hs
@@ -1,8 +1,8 @@
 module FrontPanel where
 
 import Prelude hiding (Left, Right)
-
 import Control.Monad.State
+import qualified Data.Map.Strict as Map
 
 data Wire = Up Int | Down Int | Left Int | Right Int deriving (Show)
 
@@ -12,32 +12,21 @@ parseWire ('D':n) = Down  $ read n
 parseWire ('L':n) = Left  $ read n
 parseWire ('R':n) = Right $ read n
 
-type WireState = State (Int,Int)
+type WireState = State (Int,Int,Int)
 
-data Orientation = Vertical | Horizontal
-type Plot = (Orientation,(Int,Int),(Int,Int))
+-- For a given bit of wire, return a list of points along its path and update
+-- the current position and distance
+plot :: Wire -> WireState [((Int,Int),Int)]
+plot (Up n)    = state $ \(x,y,d) -> ([ ((x, y + i), d + i) | i <- [1..n] ], (x, y + n, d + n))
+plot (Down n)  = state $ \(x,y,d) -> ([ ((x, y - i), d + i) | i <- [1..n] ], (x, y - n, d + n))
+plot (Left n)  = state $ \(x,y,d) -> ([ ((x - i, y), d + i) | i <- [1..n] ], (x - n, y, d + n))
+plot (Right n) = state $ \(x,y,d) -> ([ ((x + i, y), d + i) | i <- [1..n] ], (x + n, y, d + n))
 
-move :: Wire -> WireState ()
-move (Up n)    = modify $ \(x,y) -> (x,   y+n)
-move (Down n)  = modify $ \(x,y) -> (x,   y-n)
-move (Left n)  = modify $ \(x,y) -> (x-n, y  )
-move (Right n) = modify $ \(x,y) -> (x+n, y  )
-
-orientation :: Wire -> Orientation
-orientation (Up _)    = Vertical
-orientation (Down _)  = Vertical
-orientation (Left _)  = Horizontal
-orientation (Right _) = Horizontal
-
-plot :: Wire -> WireState Plot
-plot wire = do
-  start <- get
-  move wire
-  end <- get
-  return (start, end)
-
-plotAll :: [Wire] -> [Plot]
+-- Plots a list of wire bits, starting at the origin (0,0)
+plotAll :: [Wire] -> Map.Map (Int,Int) Int
 plotAll wires =
-  let plots = sequence $ map plot wires in
-    evalState plots (0,0)
+  let moves = map plot wires in
+    Map.fromList $ concat $ evalState (sequence moves) (0,0,0)
 
+manhattanDistance :: (Int,Int) -> Int
+manhattanDistance (x,y) = (abs x) + (abs y)
