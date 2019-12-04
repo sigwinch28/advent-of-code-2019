ab157a5c8804f2259fe9ddfb3bc4023c1d183389
Joe Harrison <joe@sigwinch.uk>
2019-12-04 14:10:51 +0000

    use library group function for day 4

--- src/FuelDepot.hs
+++ src/FuelDepot.hs
@@ -1,29 +1,20 @@
 module FuelDepot where
 
+import Data.List
+
 digits :: Int -> [Int]
 digits n = reverse $ digits' n
   where digits' n | n < 10    = [n]
                   | otherwise = n `mod` 10 : digits' (n `div` 10)
-
-runs :: Ord a => [a] -> [[a]]
-runs [] = []
-runs (x:xs) = runs' x xs [x]
-  where
-    runs' _ [] acc = [acc]
-    runs' x (y:ys) acc =
-      if x == y then
-        runs' x ys (y:acc)
-      else
-        acc : (runs' y ys [y])
 
 isSorted xs = all id $ zipWith (<=) xs (tail xs)
 
 passwordCriteria n =
   isSorted xs && hasTwoGroup xs
   where xs = digits n
-        hasTwoGroup xs = any id $ map ((>= 2) . length) $ runs xs
+        hasTwoGroup xs = any id $ map ((>= 2) . length) $ group xs
 
 enhancedPasswordCriteria n =
   isSorted xs && hasExactlyTwoGroup xs
   where xs = digits n
-        hasExactlyTwoGroup xs = any id $ map ((== 2) . length) $ runs xs
+        hasExactlyTwoGroup xs = any id $ map ((== 2) . length) $ group xs
