f606505a03f570cc37bf8811ea7a6c707bd36d17
Joe Harrison <joe@sigwinch.uk>
2019-12-04 14:26:23 +0000

    Use strings instead of int lists

--- src/FuelDepot.hs
+++ src/FuelDepot.hs
@@ -2,19 +2,13 @@ module FuelDepot where
 
 import Data.List
 
-digits :: Int -> [Int]
-digits n = reverse $ digits' n
-  where digits' n | n < 10    = [n]
-                  | otherwise = n `mod` 10 : digits' (n `div` 10)
-
-isSorted xs = all id $ zipWith (<=) xs (tail xs)
+ascending :: Ord a => [a] -> Bool
+ascending xs = and $ zipWith (<=) xs (tail xs)
 
 passwordCriteria n =
-  isSorted xs && hasTwoGroup xs
-  where xs = digits n
-        hasTwoGroup xs = any id $ map ((>= 2) . length) $ group xs
+  let digits = show n in
+    ascending digits &&  any ((>= 2) . length) (group digits)
 
 enhancedPasswordCriteria n =
-  isSorted xs && hasExactlyTwoGroup xs
-  where xs = digits n
-        hasExactlyTwoGroup xs = any id $ map ((== 2) . length) $ group xs
+  let digits = show n in
+    ascending digits &&  any ((== 2) . length) (group digits)
